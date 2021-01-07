f3155fca620afe50c4754bc3e9d37137bcac01b5
Guilherme S Beneti <guilhermesbeneti@Guilhermes-MBP.local>
2021-01-06 23:05:31 -0300

    Style-Components and Storybook implemented

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,18 @@
-const Main = () => (
-  <main>
-    <h1>React</h1>
-  </main>
+import * as S from './styles'
+
+const Main = ({
+  title = 'BoilerplateNext',
+  description = 'Boilerplate using NextJS, Typescript, Jest, Husky, Lint-Staged, Storybook'
+}) => (
+  <S.Wrapper>
+    <S.Logo src="/img/icon-512.png" alt="Boilerplate Logo React" />
+    <S.Title>{title}</S.Title>
+    <S.Description>{description}</S.Description>
+    <S.Illustration
+      src="/img/hero-illustration.svg"
+      alt="A developer hard coding"
+    />
+  </S.Wrapper>
 )
 
 export default Main
--- src/components/Main/test.tsx
+++ src/components/Main/test.tsx
@@ -6,8 +6,16 @@ describe('<Main />', () => {
   it('it should render the heading', () => {
     const { container } = render(<Main />)
 
-    expect(screen.getByRole('heading', { name: /react/i })).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', { name: /boilerplatenext/i })
+    ).toBeInTheDocument()
 
     expect(container.firstChild).toMatchSnapshot()
+  })
+
+  it('it should render colors correctly', () => {
+    const { container } = render(<Main />)
+
+    expect(container.firstChild).toHaveStyle({ 'background-color': '#06092b' })
   })
 })
